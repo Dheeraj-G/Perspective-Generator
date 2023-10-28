2380eaf85e8db0e6b7615d4f347bcc8d3663305d
Dheeraj-G <gosuladheeraj@gmail.com>
2023-10-28 09:44:14 -0400

    Update UI.js

--- UI.js
+++ UI.js
@@ -1,38 +1,3 @@
 // src/App.js
 
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
-
-// Components for Pages
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
-
-function App() {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/contact" component={Contact} />
-      </Switch>
-    </Router>
-  );
-}
-
-export default App;
